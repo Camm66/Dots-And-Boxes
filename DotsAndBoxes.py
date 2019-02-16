deede6c4f452b8cd7734b391291ba8c306b89216
Cameron Morales <cam@Camerons-iMac.local>
2019-02-15 23:48:45 -0600

    feat: completed working game functionality

--- DotsAndBoxes.py
+++ DotsAndBoxes.py
@@ -1,75 +1,74 @@
-'''
-Components
-
-1. UI
-    - Displays Current Board
-    - Gets User action
-    - Reports Results
-
-2. Board
-    - Generate board based on dimensions
-    - Represent in a 2d array?
-
-3. Intelligent Agent
-    - IE) the AI opponent
-    - Takes board state after user moves, makes a new move, returns altered board state
-    - Utilizes minimax search to make move selection
-'''
 from random import randint
 
 
 class DotsAndBoxes:
     def __init__(self, _ply, _x, _y):
-        self.playerScore = 0
-        self.aiScore = 0
         self.ply = _ply
         self.board = Board(_x, _y)
 
     def playGame(self):
         print("Dots And Boxes")
-        while True:
-            self.board.displayBoard()
-            print("Player 1 Score: %s" % self.playerScore)
-            print("Player 2 Score: %s\n" % self.aiScore)
+        while len(self.board.openVectors) > 0:
+            print("Player 1 Score: %s" % self.board.playerScore)
+            print("Player 2 Score: %s\n" % self.board.aiScore)
             print("Coordinate format == x,y,x2,y2")
             print("Hit 0 and enter to quit")
+            success = self.playerMove()
+            if success is False:
+                break
+            print("Please wait while your opponent moves...")
+            self.aiMove()
+        self.reportWinner()
+
+    def reportWinner(self):
+        if self.board.playerScore > self.board.aiScore:
+            print("You won!")
+        elif self.board.playerScore < self.board.aiScore:
+            print("The AI won!")
+        else:
+            print("The game was a draw")
+        print("Player 1 Score: %s" % self.board.playerScore)
+        print("AI Score: %s" % self.board.aiScore)
+        print("Exiting game...")
+
+    def playerMove(self):
+        while True:
             try:
+                self.board.displayBoard()
                 integers = input("Enter the coordinates of the dots you wish to connect:")
-                if integers == 0:
-                    break
+
+                if integers is 0:
+                    return False
+
                 coordinate = (integers[0], integers[1])
                 coordinate2 = (integers[2], integers[3])
-                success = self.board.move((coordinate, coordinate2))
-                if success:
-                    print("Please wait while your opponent moves...")
-                else:
+
+                success = self.board.move1((coordinate, coordinate2))
+
+                if success == 0:
+                    break
+                elif success == -1:
                     print("Invalid coordinates specified!")
             except SyntaxError:
                 print("Invalid input, please try again...")
-        print("\nExiting game...")
+        return True
+
+    def aiMove(self):
+        pass
 
     def minimax(self):
         pass
 
-
 class Board:
     def __init__(self, _m, _n):
+        self.playerScore = 0
+        self.aiScore = 0
         self.m = _m
         self.n = _n
         self.moves = (_m + 1) * (_n + 1)
         self.boxes = self.generateBoxes(_m, _n)
         self.connectedVectors = set()
         self.openVectors = self.generateVectors(_m, _n)
-        # Test Box
-        self.connectedVectors.add(((0, 0), (1, 0)))
-        self.connectedVectors.add(((1, 0), (1, 1)))
-        self.connectedVectors.add(((0, 0), (0, 1)))
-        self.connectedVectors.add(((0, 1), (1, 1)))
-
-        self.openVectors.remove(((0, 0), (1, 0)))
-        self.openVectors.remove(((1, 0), (1, 1)))
-        self.openVectors.remove(((0, 0), (0, 1)))
-        self.openVectors.remove(((0, 1), (1, 1)))
 
     def generateBoxes(self, m, n):
         cols = n
@@ -82,11 +81,13 @@ class Board:
 
     def generateVectors(self, m, n):
         vectors = set()
-        for i in range(0, m):
+        for i in range(0, m+1):
             for j in range(0, n):
-                vectors.add(((j, i), (j, i + 1)))
                 vectors.add(((j, i), (j + 1, i)))
-            vectors.add(((n, i), (n, i + 1)))
+                if i < m:
+                    vectors.add(((j, i), (j, i + 1)))
+            if i < m:
+                vectors.add(((n, i), (n, i + 1)))
         return vectors
 
     def displayBoard(self):
@@ -125,34 +126,33 @@ class Board:
             print(str1)
         print("")
 
-    def move(self, coordinates):
+    def move1(self, coordinates):
         if coordinates in self.openVectors:
             self.openVectors.discard(coordinates)
             self.connectedVectors.add(coordinates)
-            self.checkBoxes(coordinates)
-            return True
+            success = self.checkBoxes(coordinates, 1)
+            if success:
+                return 1
+            else:
+                return 0
         else:
-            return False
+            return -1
 
-    def checkBoxes(self, coordinates):
-        pass
-    '''
+    def checkBoxes(self, coordinates, owner):
+        success = False
+        for i in range(self.m):
+            for j in range(self.n):
+                box = self.boxes[i][j]
+                if coordinates in box.lines:
+                    box.connectDot(coordinates)
+                if box.complete is True and box.owner is None:
+                    box.owner = owner
+                    if owner == 1:
+                        self.playerScore += box.value
+                    success = True
+                    print("Success!")
+        return success
 
-
-
-
-    value = 0
-    box = None
-    for i in range(self.m):
-        for j in range(self.n):
-            box = boxes[i][j]
-            if coordinates in box.lines:
-                box.check(coordinates)
-            if box.complete == true:
-                value = box.value
-                break
-    return value
-    '''
 
 class Box:
     def __init__(self, _x, _y):
@@ -187,8 +187,9 @@ class Box:
         # Random number 1-5 indicating the value of this box
         self.value = randint(1, 5)
 
-    def connectDot(self, coordinate, coordinate2):
-        line = (coordinate, coordinate2)
+    def connectDot(self, coordinates):
+        line = coordinates
+        print("Connecting")
         success = False
         if line in self.lines:
             if line == self.TopLine and self.top is False:
@@ -203,4 +204,6 @@ class Box:
             elif line == self.LeftLine and self.left is False:
                 self.left = True
                 success = True
+        if self.top is True and self.right is True and self.bottom is True and self.right is True:
+            self.complete = True
         return success
